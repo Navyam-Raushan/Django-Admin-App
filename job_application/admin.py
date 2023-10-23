f708d24190fc47f8a6a8dbfb5f11efd121bcb78e
Navyam-Raushan <125335352+Navyam-Raushan@users.noreply.github.com>
2023-10-23 13:36:43 +0530

    D-53: Admin interface done, supeuser created.

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Form
 
 # Register your models here.
+# Here we need to register our database model
+
+admin.site.register(Form)
+# We need to create a superuser
+# Run python manage.py createsuperuser
+
